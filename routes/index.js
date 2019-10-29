05631ce24f069e09823c4d6c4cfabe0bbc494413
liangxiyuan <liangxiyuan@kedacom.com>
2019-10-29 16:16:15 +0800

    修改测试地址

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ module.exports = [
         method: 'GET',
         path: '/',
         handler: async (request, h) => {
-            console.log(request.yar.id);
-            return request.yar.get('user');
+            // console.log(request.yar.id);
+            // return request.yar.get('user');
+            return 'Hello World.';
         },
         config: {
             // auth: false
